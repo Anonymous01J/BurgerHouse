33100fcffbf731c177a43d6474cb6ee787deda8c
L4riUs <garnicaluis391@gmail.com>
2025-03-25 08:51:45 -0400

    eliminados controladores repetidos

--- src/controllers/C_Unidades.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Shtch\Burgerhouse\controllers;
-
-class C_Unidades extends Controller_base {
-
-    public function __construct() {
-        parent::__construct('unidades', 'Unidad');
-    }
-}
