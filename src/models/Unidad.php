a5864d99dd0f263d0fe3c17f5165b05f9b85df10
Tecrato <pinchos792003@gmail.com>
2025-04-04 14:08:03 -0400

    add nombre_like

--- src/models/Unidad.php
+++ src/models/Unidad.php
@@ -6,15 +6,21 @@ use Shtch\Burgerhouse\models;
 class Unidad extends Db_base {
     private $id;
     private $nombre;
+    private $nombre_like;
 
-    public function __construct($id = null, $nombre = null) {
+    public function __construct($id = null, $nombre = null, $nombre_like=null) {
         parent::__construct("unidades");
         $this->id = $id;
         $this->nombre = $nombre;
+        $this->nombre_like = $nombre_like;
 
         $this->add_variables([
             "a.id" => $this->id,
             "a.nombre" => $this->nombre
+        ]);
+
+        $this->add_variables_like([
+            "a.nombre" => $this->nombre_like
         ]);
 
         $this->select_query = "
