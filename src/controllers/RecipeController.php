edb504a5c232befa964a75f620142d557fba9d2c
L4riUs <148820162+L4riUs@users.noreply.github.com>
2025-04-04 14:09:15 -0400

    combo update

--- src/controllers/RecipeController.php
+++ src/controllers/RecipeController.php
@@ -5,12 +5,8 @@ use Shtch\Burgerhouse\controllers\Controller_base;
 
 class RecipeController extends Controller_base {
 
-    public function index() {
-        if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest') {
-            header('Content-Type: application/json');
-            echo json_encode(['mensaje' => 'Bienvenido al Home (AJAX)']);
-        } else {
-            include_once __DIR__ . '/../views/recipe.php';
-        }
+    public function __construct() {
+        parent::__construct("recipe");
+        // $this->db = new Recipe();
     }
 }
