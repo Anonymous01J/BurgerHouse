5b067dae51a2efc852a5a4adc13d22de0ee52f00
Tecrato <pinchos792003@gmail.com>
2025-04-04 12:21:31 -0400

    Update ComboController.php

--- src/controllers/ComboController.php
+++ src/controllers/ComboController.php
@@ -2,11 +2,12 @@
 namespace Shtch\Burgerhouse\controllers;
 use Shtch\Burgerhouse\controllers\Controller_base;
 
+use Shtch\Burgerhouse\models\Producto;
 
 class ComboController extends Controller_base {
 
     public function __construct() {
         parent::__construct("combos");
-        // $this->db = new Producto;
+        $this->db = new Producto();
     }
 }
