5bfc5d17a5a3349af6522fa54b68f280d190bd4b
Tecrato <pinchos792003@gmail.com>
2025-04-04 10:59:47 -0400

    error404

--- src/controllers/Error404Controller.php
+++ src/controllers/Error404Controller.php
@@ -1,18 +1,11 @@
 <?php
 namespace Shtch\Burgerhouse\controllers;
 
+use Shtch\Burgerhouse\controllers\Controller_base;
 
-class Error404Controller {
+class Error404Controller extends Controller_base {
 
-    public function index() {
-        // Verifica si la petición es AJAX
-        if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest') {
-            // Respuesta en JSON para AJAX
-            header('Content-Type: application/json');
-            echo json_encode(['mensaje' => 'Bienvenido al Home (AJAX)']);
-        } else {
-            // Para peticiones normales se carga la vista
-            include_once __DIR__ . '/../views/error-404.php';
-        }
+    public function __construct(){
+        parent::__construct('error-404');
     }
 }
