336ad285523f1caa3a0b251feaa17712aa517415
Tecrato <pinchos792003@gmail.com>
2025-04-04 11:25:08 -0400

    login

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -2,28 +2,42 @@
 
 namespace Shtch\Burgerhouse\controllers;
 
+use Shtch\Burgerhouse\models\Usuario;
 
-class LoginController
-{
+class LoginController extends Controller_base{
+    public $db;
+    public function __construct(){
+        parent::__construct('login');
+        $this->db = new Usuario($_POST['email'], $_POST['password']);
+    }
 
-    public function index()
-    {
-        if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest') {
-            header('Content-Type: application/json');
-            $token = $_POST['token'];
-            $secretKey = '0x4AAAAAABDYzHAap8ofRwK1xEfj_e_rKz8';
+    public function login()    {
+        header('Content-Type: application/json');
+        $token = $_POST['token'];
+        $secretKey = '0x4AAAAAABDYzHAap8ofRwK1xEfj_e_rKz8';
 
-            $response = file_get_contents("https://challenges.cloudflare.com/turnstile/v0/siteverify", false, stream_context_create([
-                'http' => [
-                    'method' => 'POST',
-                    'header' => "Content-Type: application/x-www-form-urlencoded\r\n",
-                    'content' => http_build_query(['secret' => $secretKey, 'response' => $token])
-                ]
-            ]));
-            $resultado = json_decode($response, true);
-            echo json_encode(['success' => $resultado['success']]);
-        } else {
-            include_once __DIR__ . '/../views/login.php';
+        $response = file_get_contents("https://challenges.cloudflare.com/turnstile/v0/siteverify", false, stream_context_create([
+            'http' => [
+                'method' => 'POST',
+                'header' => "Content-Type: application/x-www-form-urlencoded\r\n",
+                'content' => http_build_query(['secret' => $secretKey, 'response' => $token])
+            ]
+        ]));
+        $resultado = json_decode($response, true);
+        if (!$resultado['success']) {
+            echo json_encode(['success' => false]);
+            return;
+            die;
         }
+        $result = $this->db->search();
+        if (empty($result)) {
+            echo json_encode(['success' => false]);
+            return;
+            die;
+        }
+        echo json_encode(['success' => true]);
+        return;
+        die;
+
     }
 }
