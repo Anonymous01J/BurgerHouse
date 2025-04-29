6f4c5cc4b8142f190103d051ef69ac77d18fce79
Tecrato <pinchos792003@gmail.com>
2025-04-29 17:12:36 -0400

    deberia ingresar el rol

--- src/controllers/RolController.php
+++ src/controllers/RolController.php
@@ -1,13 +1,29 @@
 <?php
 namespace Shtch\Burgerhouse\controllers;
 use Shtch\Burgerhouse\controllers\Controller_base;
+use Shtch\Burgerhouse\models\Permiso;
 use Shtch\Burgerhouse\models\Rol;
+use Shtch\Burgerhouse\models\Permiso_rol;
 
 class RolController extends Controller_base {
 
     public function __construct() {
         parent::__construct("roles");
         $this->db = new Rol();
+    }
+
+    public function agregar_rol(){
+        $nombre = $_POST['nose']['detalles']['nombre'];
+        $descripcion = $_POST['nose']['detalles']['descripcion'];
+        $db = new Rol(nombre: $nombre, descripcion: $descripcion);
+        $last_id = $db->agregar();
+
+        $lista_permisos = $_POST['nose']['permisos'];
+        
+        for ($i = 0; $i < count($lista_permisos); $i++) {
+            $db= new  Permiso_rol(id_rol: $last_id, modulo: $lista_permisos[$i]['modulo'], permisos: $lista_permisos[$i]['permisos']);
+            $db->agregar();
+        }
     }
     
 }
--- src/models/Rol.php
+++ src/models/Rol.php
@@ -6,21 +6,25 @@ use Shtch\Burgerhouse\models\Db_base;
 class Rol extends Db_base {
     private $id;
     private $nombre;
+    private $descripcion;
 
-    public function __construct($id = null, $nombre = null) {
+    public function __construct($id = null, $nombre = null, $descripcion = null) {
         parent::__construct("roles");
         
         $this->id = $id;
         $this->nombre = $nombre;
+        $this->descripcion = $descripcion;
 
         $this->add_variables([
             "a.id" => $this->id,
-            "a.nombre" => $this->nombre
+            "a.nombre" => $this->nombre,
+            "a.descripcion" => $this->descripcion
         ]);
         
         $this->select_query = "
             a.id,
-            a.nombre
+            a.nombre,
+            a.descripcion
         ";
     }
 }
