deade70a14f1a5a04a617923958a539378dcf589
Tecrato <pinchos792003@gmail.com>
2025-04-29 17:48:07 -0400

    Update Rol.php

--- src/models/Rol.php
+++ src/models/Rol.php
@@ -7,18 +7,24 @@ class Rol extends Db_base {
     private $id;
     private $nombre;
     private $descripcion;
+    private $nombre_like;
 
-    public function __construct($id = null, $nombre = null, $descripcion = null) {
+    public function __construct($id = null, $nombre = null, $descripcion = null, $nombre_like=null) {
         parent::__construct("roles");
         
         $this->id = $id;
         $this->nombre = $nombre;
         $this->descripcion = $descripcion;
+        $this->nombre_like = $nombre_like;
 
         $this->add_variables([
             "a.id" => $this->id,
             "a.nombre" => $this->nombre,
             "a.descripcion" => $this->descripcion
+        ]);
+
+        $this->add_variables([
+            "a.nombre LIKE" => $this->nombre_like
         ]);
         
         $this->select_query = "
