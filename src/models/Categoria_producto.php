bfb634591c8a44c4160c30d369055fd26a96bb7c
Tecrato <pinchos792003@gmail.com>
2025-05-01 17:26:18 -0400

    Update Categoria_producto.php

--- src/models/Categoria_producto.php
+++ src/models/Categoria_producto.php
@@ -6,14 +6,12 @@ use Shtch\Burgerhouse\models\Db_base;
 class Categoria_producto extends Db_base {
     private $id;
     private $nombre;
-    private $tipo;
     private $active;
     private $nombre_like;
 
     public function __construct(
         $id = null,
         $nombre = null,
-        $tipo = null,
         $active = null,
         $nombre_like = null
     ) {
@@ -21,14 +19,12 @@ class Categoria_producto extends Db_base {
         
         $this->id = $id;
         $this->nombre = $nombre;
-        $this->tipo = $tipo;
         $this->active = $active;
         $this->nombre_like = $nombre_like;
 
         $this->add_variables([
             "a.id" => $this->id,
             "a.nombre" => $this->nombre,
-            "a.tipo" => $this->tipo,
             "a.active" => $this->active
         ]);
 
@@ -39,7 +35,6 @@ class Categoria_producto extends Db_base {
         $this->select_query = "
             a.id,
             a.nombre,
-            a.tipo,
             a.active
         ";
     }
