b3472c6eabd6b02aafd48b9ea8daf1b93a87c72c
Tecrato <pinchos792003@gmail.com>
2025-05-01 17:24:32 -0400

    Update CategoryProductoController.php

--- src/controllers/CategoryProductoController.php
+++ src/controllers/CategoryProductoController.php
@@ -6,7 +6,7 @@ use Shtch\Burgerhouse\models\Categoria_producto;
 
 class CategoryProductoController extends Controller_base {
     public function __construct() {
-        parent::__construct("category_producto");
+        parent::__construct("category_productos");
         $this->db = new Categoria_producto();
     }
     
