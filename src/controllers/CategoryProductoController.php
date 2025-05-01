559d0534ef88421b73b41549922fba32d2597161
Tecrato <pinchos792003@gmail.com>
2025-05-01 17:46:32 -0400

    Update CategoryProductoController.php

--- src/controllers/CategoryProductoController.php
+++ src/controllers/CategoryProductoController.php
@@ -1,13 +1,13 @@
 <?php
 namespace Shtch\Burgerhouse\controllers;
 use Shtch\Burgerhouse\controllers\Controller_base;
-use Shtch\Burgerhouse\models\Categoria_materia_prima;
+use Shtch\Burgerhouse\models\Categoria_producto;
 
 
-class CategoryMateriaPrimaController extends Controller_base {
+class CategoryProductoController extends Controller_base {
     public function __construct() {
-        parent::__construct("category_materia_prima");
-        $this->db = new Categoria_materia_prima();
+        parent::__construct("categorias_productos");
+        $this->db = new Categoria_producto();
     }
     
 }
