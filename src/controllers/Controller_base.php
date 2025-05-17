47ad836c9c7f05a5562ff94d3ee0a55bd6791a2d
Tecrato <pinchos792003@gmail.com>
2025-05-17 12:51:30 -0400

    cualquier modulo deberia poder registrar imagen

--- src/controllers/Controller_base.php
+++ src/controllers/Controller_base.php
@@ -34,6 +34,9 @@ class Controller_base {
         try {
             $this->db->clear();
             $this->db->__construct(...$_POST);
+            if (isset($_FILES['imagen'])){
+                $this->guardar_imagen_single();
+            }
             echo json_encode(['success' => true, 'last_id' => $this->db->agregar()]);
         } catch (Exception $e) {
             echo json_encode(['success' => false, 'message' => $e->getMessage()]);
@@ -45,6 +48,9 @@ class Controller_base {
         try {
             for ($i = 0; $i < count($_POST['lista']); $i++) {
                 $this->db->__construct(...$_POST['lista'][$i]);
+                if (isset($_FILES['imagen'])) {
+                    $this->guardar_imagen_mult($i);
+                }
                 $this->db->agregar();
             }
             echo json_encode(['success' => true]);
@@ -75,6 +81,9 @@ class Controller_base {
             $this->db->clear();
             $this->db->__construct(...$_POST);
             $result = $this->db->actualizar();
+            if (isset($_FILES['imagen'])) {
+                $this->guardar_imagen_single();
+            }
             if ($result == false or $result == 0) {
                 echo json_encode(['success' => false, 'message' => 'No se pudo actualizar el registro']);
             } else {
@@ -85,6 +94,14 @@ class Controller_base {
         }
     }
 
+    public function guardar_imagen_mult($index) {
+        $imagen = $_FILES['lista'];
+        move_uploaded_file($imagen['tmp_name'][$index]['imagen'],'../src/media/'.$this->module_name.'/'.$imagen['name'][$index]['imagen']);
+    }
+    public function guardar_imagen_single() {
+        $imagen = $_FILES['imagen'];
+        move_uploaded_file($imagen['tmp_name'],'../src/media/'.$this->module_name.'/'.$imagen['name']);
+    }
 
     public function check(...$args) {
         echo "<pre>";
--- src/controllers/ProductController.php
+++ src/controllers/ProductController.php
@@ -11,55 +11,46 @@ class ProductController extends Controller_base {
         $this->db = new Producto();
     }
 
-    public function add() {
-        header('Content-Type: application/json');
-        try {
-            $this->db->clear();
-            $this->db->__construct(...$_POST);
-            $this->guardar_imagen($_POST['nombre'],0);
-            echo json_encode(['success' => true, 'last_id' => $this->db->agregar()]);
-        } catch (Exception $e) {
-            echo json_encode(['success' => false, 'message' => $e->getMessage()]);
-        }
-    }
-    public function add_many() {
-        // header('Content-Type: application/json');
-        try {
-            for ($i = 0; $i < count($_POST['lista']); $i++) {
-                $this->guardar_imagen($_POST['lista'][$i]['nombre'],$i);
-                $this->db->__construct(...$_POST['lista'][$i]);
-                $this->db->agregar();
-            }
-            echo json_encode(['success' => true ]);
-        } catch (Exception $e) {
-            echo json_encode(['success' => false, 'message' => $e->getMessage()]);
-        }
-    }
-    public function update() {
-        header('Content-Type: application/json');
-        try {
-            $this->db->clear();
-            $this->db->__construct(...$_POST);
-            $result = $this->db->actualizar();
-            if (isset($_FILES['imagen'])) {
-                $this->guardar_imagen_single();
-            }
-            if ($result == false or $result == 0) {
-                echo json_encode(['success' => false, 'message' => 'No se pudo actualizar el registro']);
-            } else {
-                echo json_encode(['success' => true]);
-            }
-        } catch (Exception $e) {
-            echo json_encode(['success' => false, 'message' => $e->getMessage()]);
-        }
-    }
-
-    public function guardar_imagen($pre,$index) {
-        $imagen = $_FILES['lista'];
-        move_uploaded_file($imagen['tmp_name'][$index]['imagen'],'../src/media/productos/'.$imagen['name'][$index]['imagen']);
-    }
-    public function guardar_imagen_single() {
-        $imagen = $_FILES['imagen'];
-        move_uploaded_file($imagen['tmp_name'],'../src/media/productos/'.$imagen['name']);
-    }
+    // public function add() {
+    //     header('Content-Type: application/json');
+    //     try {
+    //         $this->db->clear();
+    //         $this->db->__construct(...$_POST);
+    //         $this->guardar_imagen_mult(0);
+    //         echo json_encode(['success' => true, 'last_id' => $this->db->agregar()]);
+    //     } catch (Exception $e) {
+    //         echo json_encode(['success' => false, 'message' => $e->getMessage()]);
+    //     }
+    // }
+    // public function add_many() {
+    //     // header('Content-Type: application/json');
+    //     try {
+    //         for ($i = 0; $i < count($_POST['lista']); $i++) {
+    //             $this->guardar_imagen_mult($i);
+    //             $this->db->__construct(...$_POST['lista'][$i]);
+    //             $this->db->agregar();
+    //         }
+    //         echo json_encode(['success' => true ]);
+    //     } catch (Exception $e) {
+    //         echo json_encode(['success' => false, 'message' => $e->getMessage()]);
+    //     }
+    // }
+    // public function update() {
+    //     header('Content-Type: application/json');
+    //     try {
+    //         $this->db->clear();
+    //         $this->db->__construct(...$_POST);
+    //         $result = $this->db->actualizar();
+    //         if (isset($_FILES['imagen'])) {
+    //             $this->guardar_imagen_single();
+    //         }
+    //         if ($result == false or $result == 0) {
+    //             echo json_encode(['success' => false, 'message' => 'No se pudo actualizar el registro']);
+    //         } else {
+    //             echo json_encode(['success' => true]);
+    //         }
+    //     } catch (Exception $e) {
+    //         echo json_encode(['success' => false, 'message' => $e->getMessage()]);
+    //     }
+    // }
 }
