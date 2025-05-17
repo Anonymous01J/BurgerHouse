031137c6504ad60f17c3ddc74bd5acbda6852ff5
Tecrato <pinchos792003@gmail.com>
2025-05-17 11:15:55 -0400

    receta

--- src/models/Receta.php
+++ src/models/Receta.php
@@ -7,16 +7,19 @@ class Receta extends Db_base {
     private $id;
     private $nombre;
     private $nombre_like;
+    private $id_producto;
 
-    public function __construct($id = null, $nombre = null, $nombre_like = null) {
+    public function __construct($id = null, $nombre = null, $nombre_like = null, $id_producto = null) {
         parent::__construct("recetas");
         $this->id = $id;
         $this->nombre = $nombre;
         $this->nombre_like = $nombre_like;
+        $this->id_producto = $id_producto;
 
         $this->add_variables([
             "a.id" => $this->id,
-            "a.nombre" => $this->nombre
+            "a.nombre" => $this->nombre,
+            "a.id_producto" => $this->id_producto,
         ]);
 
         $this->add_variables_like([
@@ -25,7 +28,8 @@ class Receta extends Db_base {
 
         $this->select_query = "
             a.id,
-            a.nombre
+            a.nombre,
+            a.id_producto
         ";
     }
 }
