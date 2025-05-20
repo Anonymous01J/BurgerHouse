aa4a7ea54aca4acff0e482695f8d2ae4b555e103
Tecrato <pinchos792003@gmail.com>
2025-05-20 19:12:11 -0400

    Update OrderController.php

--- src/controllers/OrderController.php
+++ src/controllers/OrderController.php
@@ -2,10 +2,12 @@
 namespace Shtch\Burgerhouse\controllers;
 use Shtch\Burgerhouse\controllers\Controller_base;
 
+use Shtch\Burgerhouse\models\Orden;
 
 class OrderController extends Controller_base {
 
     public function __construct() {
         parent::__construct("orders");
+        $this->db = new Orden();
     }
 }
