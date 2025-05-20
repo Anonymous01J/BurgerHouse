3dc0e2762c30e0889c9327cbcc0734d6ac3f2b2c
Tecrato <pinchos792003@gmail.com>
2025-05-20 19:24:25 -0400

    tipo de orden

--- src/models/Orden.php
+++ src/models/Orden.php
@@ -8,12 +8,14 @@ class Orden extends Db_base {
     private $numero;
     private $id_cliente;
     private $fecha;
+    private $tipo;
 
     public function __construct(
         $id = null,
         $numero = null,
         $id_cliente = null,
         $fecha = null,
+        $tipo = null
         
     ) {
         parent::__construct("pagos");
@@ -22,6 +24,7 @@ class Orden extends Db_base {
         $this->numero = $numero;
         $this->id_cliente = $id_cliente;
         $this->fecha = $fecha;
+        $this->tipo = $tipo;
 
         if ($this->numero == null){
             $this->numero = random_int(10000000,99999999);
@@ -31,7 +34,8 @@ class Orden extends Db_base {
             "a.id" => $this->id,
             "a.numero" => $this->numero,
             "a.id_cliente" => $this->id_cliente,
-            "a.fecha" => $this->fecha
+            "a.fecha" => $this->fecha,
+            "a.tipo" => $this->tipo
         ]);
     }
 }
